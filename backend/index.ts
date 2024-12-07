2aa4fbdcaa22ed6bafff8c6ae868d75839fe9345
Siddhika Phule <phulesiddhika@gmail.com>
2024-12-07 19:59:07 +0530

    project complete

--- backend/index.ts
+++ backend/index.ts
@@ -3,18 +3,11 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import newsRouter from './src/routes/newsRoutes';
-import * as admin from 'firebase-admin';
 import { Request, Response } from 'express';
 
 const bodyparser = require('body-parser');
 
-const credentials = require('./serviceAccountKey.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(credentials)
-  });
-
-const MONGO_URI = process.env.MONGO_URL!;
+const MONGO_URI = process.env.MONGO_URL;
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -29,10 +22,10 @@ app.use(bodyparser.json());
 
 
 if (!MONGO_URI) {
-    console.error('MongoDB connection URI is not defined in environment variables');
+    console.error('MongoDB connection URI is not defined in environment variables..');
     process.exit(1); // Exit the process with an error code
 }
-console.log('MongoDB URL:', process.env.MONGO_URL); // For debugging
+// console.log('MongoDB URL:', process.env.MONGO_URL); // For debugging
 
 mongoose.connect(process.env.MONGO_URL as string)
   .then(() => console.log('Connected to MongoDB successfully'))
